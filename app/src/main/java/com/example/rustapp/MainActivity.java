ffe770931286b8266e01e4dc48710e018a76256a
shrey-md21 <m.d.shreyas210@gmail.com>
2024-02-14 09:54:59 +0530

    background setup

--- app/src/main/java/com/example/rustapp/MainActivity.java
+++ app/src/main/java/com/example/rustapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.rustapp;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.materialswitch.MaterialSwitch;
@@ -13,12 +17,13 @@ import com.google.android.material.materialswitch.MaterialSwitch;
 import java.io.File;
 import java.io.FileOutputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Application.ActivityLifecycleCallbacks {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        getApplication().registerActivityLifecycleCallbacks(this);
 
         ((MaterialSwitch) findViewById(R.id.serviceToggle))
                 .setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -40,5 +45,53 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Failed to clear logs!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    @Override
+    protected void onDestroy() {
+
+        Toast.makeText(this, "MainActivity is being destroyed", Toast.LENGTH_SHORT).show();
+        getApplication().unregisterActivityLifecycleCallbacks(this);
+        super.onDestroy();
+
+    }
+    private int numStarted = 0;
+    @Override
+    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+
+    }
+    @Override
+    public void onActivityStarted(@NonNull Activity activity) {
+        numStarted++;
+        if (numStarted == 1) {
+            Log.d("RustService", "App is in foreground state");
+        }
+    }
+
+    @Override
+    public void onActivityResumed(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(@NonNull Activity activity) {
+        numStarted--;
+        if (numStarted == 0) {
+            Log.d("RustService", "App is in background state");
+        }
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(@NonNull Activity activity) {
+
     }
 }
--- app/src/main/java/com/example/rustapp/RustService.java
+++ app/src/main/java/com/example/rustapp/RustService.java
@@ -1,16 +1,39 @@
 package com.example.rustapp;
 
+import android.app.Activity;
+import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class RustService extends Service {
-    private static final String CHANNEL_ID = "RustServiceChannel";
+
+//    private boolean running = false;
+    private static final String DEFAULT_CHANNEL_ID = "RustServiceChannel";
     private static final int NOTIFICATION_ID = 1;
+    private static final String LOG_FILE_NAME = "rust_logs.txt";
 
     static {
         System.loadLibrary("rustapp");
@@ -21,22 +44,37 @@ public class RustService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+//        running = true;
+//        MyThread thread = new MyThread();
+//        thread.start();
+        String logMessages = "Service Created";
+        Log.d("RustService",logMessages);
+        appendLog(logMessages);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
+        String logMessages = "Service Started";
+        Log.d("RustService",logMessages);
+        appendLog(logMessages);
 
+        // create and display notification
         createNotificationChannel();
         startForeground(NOTIFICATION_ID, buildNotification());
 
+        // service is started
         startService(this.getFilesDir().toString());
 
-        return START_STICKY;
+        return Service.START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+//        running = false;
+        String logMessages = "Service Stopped/Closed";
+        Log.d("RustService", logMessages);
+        appendLog(logMessages);
         super.onDestroy();
     }
 
@@ -47,8 +85,8 @@ public class RustService extends Service {
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
-                    "Rust Service Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel(DEFAULT_CHANNEL_ID,
+                    "RustServiceChannel", NotificationManager.IMPORTANCE_DEFAULT);
 
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
@@ -58,14 +96,64 @@ public class RustService extends Service {
     private Notification buildNotification() {
         Notification.Builder builder;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            builder = new Notification.Builder(this, CHANNEL_ID);
+            builder = new Notification.Builder(this, DEFAULT_CHANNEL_ID);
         } else {
             builder = new Notification.Builder(this);
         }
 
-        builder.setContentTitle("Rust Service").setContentText("Running...")
-                .setSmallIcon(R.mipmap.ic_launcher).setPriority(Notification.PRIORITY_DEFAULT);
+        builder.setContentTitle("Rust Service")
+                .setContentText("Running...")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(Notification.PRIORITY_DEFAULT);
+
+        String logMessages = "Notification built and sent";
+        Log.d("RustService", logMessages);
+        appendLog(logMessages);
 
         return builder.build();
     }
+
+    private void appendLog(String logMessages) {
+
+        File logFile = new File(getFilesDir(), LOG_FILE_NAME);
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            buf.append(logMessages);
+            buf.newLine();
+            buf.close();
+
+        } catch (IOException e) {
+            Log.e("RustService", "Error writing to log file: " + e.getMessage());
+        }
+    }
+//    private String getCurrentTimeStamp() {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        return sdf.format(new Date());
+//    }
+
+//    class MyThread extends Thread {
+//        public void run() {
+//            int i = 0;
+//            while(running) {
+//                try {
+//                    Thread.sleep(10000);
+//                    String logMessages = "Thread running. Count: " + i;
+//                    Log.d("RustService", logMessages);
+//                    appendLog(logMessages);
+//                    i++;
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+
 }
